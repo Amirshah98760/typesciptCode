d2001f96a03793400d5dfa3056497368725eb0fb
Amirshah98760 <amirshah98760@gmail.com>
2026-02-02 11:44:23 +0500

    First code in typescript

---
+++ class1/app.js
@@ -0,0 +1,10 @@
+// export {};
+var userName = 'Amir shah';
+console.log(userName);
+var str = "Hello world";
+console.log(str);
+var num = 42;
+console.log(num);
+// for (let i = 0; i < 5; i++) {
+//     console.log(i);
+// }
---
+++ class1/app.ts
@@ -0,0 +1,18 @@
+// export {};
+
+var userName: string = 'Amir shah';
+console.log(userName);
+
+var str: string = "Hello world";
+console.log(str);
+
+
+var num : number = 42;
+console.log(num);
+
+
+
+
+// for (let i = 0; i < 5; i++) {
+//     console.log(i);
+// }
